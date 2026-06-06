fb92f0b5fe68d704496aa54a06d2f3e855b9de8e
Turtlemaster890 <mastepieter16@gmail.com>
2026-06-05 20:45:52 -0400

    Release v0.4.4

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ export const DEBUG_MODE = {
     disableOptimization: false,
 }
 
-export const VERSION = "0.4.3"
+export const VERSION = "0.4.4"
 
 //function for spamming debug print statements
 //its faster to type and i can search and destroy them after im done debugging without having to worry about nuking actually important log messages
